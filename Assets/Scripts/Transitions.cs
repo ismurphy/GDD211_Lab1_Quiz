0cb3f92d31f9091690e9da73d064820380c06d79
ismurphy <78505720+ismurphy@users.noreply.github.com>
2021-02-10 16:25:26 -0500

    Quiz edits
    Here is my quiz

--- Assets/Scripts/Transitions.cs
+++ Assets/Scripts/Transitions.cs
@@ -7,17 +7,21 @@
 
 public class Transitions : MonoBehaviour
 {
-	[SerializeField] private Animator puppy1Animator;
+    [SerializeField] private Animator puppy1Animator;
+    [SerializeField] private float walkSpeed;
 
-	private void Update()
-	{
-		if (Input.GetAxisRaw("Vertical") > 0f) //Walk
-		{
-			transform.position += new Vector3(Time.deltaTime * 0.3f, 0f);
-		}
-		else //Idle
-		{
+    private void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        if (Input.GetAxisRaw("Vertical") > 0f) //Walk
+        {
+            transform.position += new Vector3(Time.deltaTime * 0.3f, 0f);
+        }
+        else if (input < 0) //Idle
+        {
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0f, 90f, 0f), Time.deltaTime * 4f);
+            puppy1Animator.SetFloat("walkSpeed", Mathf.Abs(input));
 
-		}
-	}
+        }
+    }
 }
